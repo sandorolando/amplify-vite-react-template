d2a0dbf0c986a316774134f06f42eb3b1761fb66
sandorolando <rolando.sandor@gmail.com>
2024-08-20 12:30:35 -0600

    Just registration page.

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { Routes, Route } from 'react-router-dom';
 import SplashPage from './pages/SplashPage';
 import HomePage from './pages/HomePage';
 import RegisterPage from './pages/RegisterPage';
-import { Authenticator } from '@aws-amplify/ui-react';
 import { Amplify } from 'aws-amplify';
 import outputs from '../amplify_outputs.json';
 import '@aws-amplify/ui-react/styles.css';
@@ -13,19 +12,7 @@ const App = () => {
   return (
     <Routes>
       <Route path="/" element={<SplashPage />} />
-      <Route
-        path="/home"
-        element={
-          <Authenticator>
-            {({ signOut, user }) => (
-              <div>
-                <HomePage />
-                <button onClick={signOut}>Sign out</button>
-              </div>
-            )}
-          </Authenticator>
-        }
-      />
+      <Route path="/home" element={<HomePage/>}/>
       <Route path="/register" element={<RegisterPage />} />
     </Routes>
   );
