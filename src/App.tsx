0dcad5016592307f0a51610c6949cc7af352c637
sandorolando <rolando.sandor@gmail.com>
2024-08-20 11:42:58 -0600

    Added storage

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { Routes, Route } from 'react-router-dom';
 import SplashPage from './pages/SplashPage';
 import HomePage from './pages/HomePage';
 import RegisterPage from './pages/RegisterPage';
-import ProtectedRoute from './components/ProtectedRoute';
 import { Authenticator } from '@aws-amplify/ui-react';
 import { Amplify } from 'aws-amplify';
 import outputs from '../amplify_outputs.json';
@@ -13,23 +12,23 @@ Amplify.configure(outputs);
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<SplashPage />} />
-      <Route
-        path="/home"
-        element={
-          <Authenticator>
-            {({ signOut, user }) => (
-              <ProtectedRoute>
-                <HomePage />
-                <button onClick={signOut}>Sign out</button>
-              </ProtectedRoute>
-            )}
-          </Authenticator>
-        }
-      />
-      <Route path="/register" element={<RegisterPage />} />
-    </Routes>
-  );
+    <Route path="/" element={<SplashPage />} />
+    <Route
+      path="/home"
+      element={
+        <Authenticator>
+          {({ signOut, user }) => (
+            <div> {/* Added a wrapper <div> here */}
+              <HomePage />
+              <button onClick={signOut}>Sign out</button>
+            </div>
+          )}
+        </Authenticator>
+      }
+    />
+    <Route path="/register" element={<RegisterPage />} />
+  </Routes>
+);
 };
 
 export default App;
