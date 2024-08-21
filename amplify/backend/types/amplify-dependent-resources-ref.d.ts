4890bb1779879051bf0e9833e6b78243f7056dfe
sandorolando <rolando.sandor@gmail.com>
2024-08-20 20:06:51 -0600

    What the fuck will this do?

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "userweb": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
